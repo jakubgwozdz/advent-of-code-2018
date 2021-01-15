57497b74cb861c6a2d35ee2396ce8b3c1b801a17
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2021-01-15 18:42:29 +0100

    day 6 part 1 ok

--- day06.py
+++ day06.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 from day02 import Counter
 
 
@@ -12,43 +14,95 @@ def parse(line):
     return int(ax), int(ay)
 
 
+def extend(r, v):
+    if v in r:
+        return r
+    elif v < r.start:
+        return range(v, r.stop)
+    elif v >= r.stop:
+        return range(r.start, v + 1)
+
+
 def part1(lines):
-    coords = list(parse(line) for line in lines)
+    coords = [parse(line) for line in lines]
+    x0 = coords[0][0]
+    y0 = coords[0][1]
+    range_x = range(x0, x0 + 1)
+    range_y = range(y0, y0 + 1)
     for c in coords:
-        print(c)
-    # list(map(lambda line: list(map(lambda d: int(d), line.split(','))), lines))
-    min_x = min(coords, key=lambda k: k[0])[0]
-    min_y = min(coords, key=lambda k: k[1])[1]
-    max_x = max(coords, key=lambda k: k[0])[0]
-    max_y = max(coords, key=lambda k: k[1])[1]
-    print("x: {}-{}, y: {}-{}".format(min_x, max_x, min_y, max_y))
+        (x, y) = c
+        range_x = extend(range_x, x)
+        range_y = extend(range_y, y)
+
+    print("x: {}, y: {}".format(range_x, range_y))
     counter = Counter()
 
-    for x in range(min_x, max_x):
-        for y in range(min_y, max_y):
-            distances = iter((c, dist(c, (x, y))) for c in coords)
-            min_dist = min(distances, key=lambda c: c[1])
-            if not any(filter(lambda c: c[1] == min_dist[1] and c != min_dist, distances)):
-                counter[tuple(min_dist[0])] += 1
+    queue = deque()
+    dists = {}
 
-    for x in range(min_x, max_x):
-        min_dist = min(((c, dist(c, (x, min_y - 1))) for c in coords), key=lambda c: c[1])
-        counter[tuple(min_dist[0])] = 0
-        min_dist = min(((c, dist(c, (x, max_y + 1))) for c in coords), key=lambda c: c[1])
-        counter[tuple(min_dist[0])] = 0
-    for y in range(min_y, max_y):
-        min_dist = min(((c, dist(c, (min_x - 1, y))) for c in coords), key=lambda c: c[1])
-        counter[tuple(min_dist[0])] = 0
-        min_dist = min(((c, dist(c, (max_x + 1, y))) for c in coords), key=lambda c: c[1])
-        counter[tuple(min_dist[0])] = 0
+    for c in coords:
+        queue.append(c)
+        dists[c] = (0, {c})
 
-    return counter[max(counter, key=lambda c: counter[c])]
+    while len(queue) > 0:
+        # print("distances: {}, queue: {}".format(len(dists), queue))
+
+        c = queue.popleft()
+        root = dists[c]
+        # print("processing {} root {}".format(c, root))
+        (x, y) = c
+        if x in range_x:
+            process((x + 1, y), root, dists, queue)
+            process((x - 1, y), root, dists, queue)
+        if y in range_y:
+            process((x, y + 1), root, dists, queue)
+            process((x, y - 1), root, dists, queue)
+
+    print("finished with queue")
+
+    for x in range_x:
+        for y in range_y:
+            (_, closest) = dists[(x, y)]
+            if len(closest) == 1:
+                counter[next(iter(closest))] += 1
+
+    print("calculated area")
+    print(counter)
+
+    for x in range_x:
+        cleanup_infinities((x, range_y.start), counter, dists)
+        cleanup_infinities((x, range_y.stop - 1), counter, dists)
+    for y in range_y:
+        cleanup_infinities((range_x.start, y), counter, dists)
+        cleanup_infinities((range_x.stop - 1, y), counter, dists)
+
+    return counter[max(counter, key=lambda v: counter[v])]
 
 
-def dist(p1, p2):
-    (x1, y1) = p1
-    (x2, y2) = p2
-    return abs(x1 - x2) + abs(y1 - y2)
+def cleanup_infinities(c, counter, dists):
+    (_, closest) = dists[c]
+    if len(closest) == 1:
+        counter[next(iter(closest))] = 0
+
+
+def process(c1, root, distances, queue):
+    (d, rs) = root
+    if d < 0:
+        return
+    (d1, rs1) = v1 = (d + 1, rs)
+    if c1 in distances:
+        (d0, rs0) = distances[c1]
+        if d0 < d1:
+            pass
+        elif d0 > d1:
+            pass
+        elif rs0 == rs:
+            pass
+        else:
+            distances[c1] = (d0, rs0 | rs)
+    else:
+        distances[c1] = v1
+        queue.append(c1)
 
 
 def part2(i):
