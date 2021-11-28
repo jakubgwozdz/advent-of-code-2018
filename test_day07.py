926baf7b90307d1a8f549557b5cc2d6324165c60
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2021-11-28 22:19:37 +0100

    don't remember where I stopped...

--- test_day07.py
+++ test_day07.py
@@ -22,6 +22,13 @@ Step F must be finished before step E can begin.
 
     def test_example2(self):
         i = """\
+Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.
 """.splitlines()
         self.assertEqual(None, part2(i))
 
